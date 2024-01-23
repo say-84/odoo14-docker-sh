10d6292b5441c8b60e87725ca11d8f6b91a14706
say-84 <46970000+say-84@users.noreply.github.com>
2024-01-23 03:47:13 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/elblasy33/odoo15-d0cker-sh.git $DESTINATION
+git clone --depth=1 https://github.com/say-84/odoo14-docker-sh.git $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
@@ -11,9 +11,9 @@ sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10015/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20015/'$CHAT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/10014/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20014/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: Elblasy2022@1234 | Live chat port: '$CHAT
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: Sayed@2020 | Live chat port: '$CHAT
